e7d20af4876a8eff19a164fa3a68a8428eae84dd
luke <luke.w@topskyweb.com>
2017-08-23 14:41:58 +0800

    appadapter

--- app/src/main/java/com/shuhao/androidbeatupdate/MainActivity.java
+++ app/src/main/java/com/shuhao/androidbeatupdate/MainActivity.java
@@ -3,11 +3,14 @@ package com.shuhao.androidbeatupdate;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.shuhao.update.UpdateManager;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        UpdateManager.getInstance(this).downloadApk("http://app.appyunpan.com//yyhmobile//yyhmobile-d.apk","test.apk").isOpenApk(true);
     }
 }
---
+++ update/src/main/java/com/shuhao/update/UpdateManager.java
@@ -0,0 +1,81 @@
+package com.shuhao.update;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
+
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.FileCallBack;
+
+import java.io.File;
+
+import okhttp3.Call;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by luke on 2017/8/23.
+ */
+
+public class UpdateManager {
+
+    private static UpdateManager instance;
+
+    private boolean isOpenApk = false;
+
+    private Context context;
+
+    public static UpdateManager getInstance(Context context) {
+        if (instance == null) {
+            instance = new UpdateManager(context);
+        }
+        return instance;
+    }
+
+    private UpdateManager(Context context){
+        this.context = context;
+    }
+
+    public UpdateManager downloadApk(String url, final String apkName) {
+        OkHttpUtils.get().url(url).build().execute(new FileCallBack(Environment.getExternalStorageDirectory().getAbsolutePath(), apkName) {
+            @Override
+            public void onError(Call call, Exception e, int id) {
+                Log.e(TAG, "downloadApk:" + e.getMessage());
+            }
+
+            @Override
+            public void onResponse(File response, int id) {
+                if(isOpenApk){
+                    openFile(Environment.getExternalStorageDirectory().getAbsolutePath() + "/" + apkName);
+                }
+                Log.e(TAG, "download succ");
+            }
+
+            @Override
+            public void inProgress(float progress, long total, int id) {
+                Log.e(TAG, "downloadInProgress: " + (int) (100 * progress));
+            }
+        });
+        return getInstance(context);
+    }
+
+    private void openFile(String fileName) {
+        File file = new File(fileName);
+        // TODO Auto-generated method stub
+        Log.e("OpenFile", file.getName());
+        Intent intent = new Intent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        intent.setAction(android.content.Intent.ACTION_VIEW);
+        intent.setDataAndType(Uri.fromFile(file),
+                "application/vnd.android.package-archive");
+        context.startActivity(intent);
+    }
+
+    public void isOpenApk(boolean isOpenApk) {
+        this.isOpenApk = isOpenApk;
+    }
+
+
+}
